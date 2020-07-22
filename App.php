1b6b2607110f61ccf47a925b4a3dd0053ce14003
vasu-tamrakar <vasu.tamrakar@cssindiaonline.com>
2020-07-22 16:48:17 +0530

    fixed

--- App.php
+++ App.php
@@ -6,26 +6,27 @@ class App{
     public function getUser(){
         echo 'First Name: '.$this->first_name.'<br>Last Name: '.$this->last_name;
         echo '<br>'.$this->age;
+        $max="Max from 172222";
         if($this->age > 17){
-            echo "Max from 172222";
+            echo $max;
         }
         if($this->age > 17){
-            echo "Max from 172222";
+            echo $max;
         }
         if($this->age > 17){
-            echo "Max from 172222";
+            echo $max;
         }
         if($this->age > 17){
-            echo "Max from 172222";
+            echo $max;
         }
         if($this->age > 17){
-            echo "Max from 172222";
+            echo $max;
         }
         if($this->age > 17){
-            echo "Max from 17";
+            echo $max;
         }        echo '<br>';
         if($this->age < 17){
-            echo "Min from 172222";
+            echo $max;
         } 
     }
     public function setUser($fname=false,$lname=false){
@@ -33,7 +34,7 @@ class App{
         $this->last_name = ($lname)?$lname:'N/A'; 
     }
 
-    public function setUser1($fname=false,$lname=false, $age){
+    public function setUser1($fname=false,$lname=false, $age=false){
         $this->first_name = ($fname)?$fname:'N/A';
         $this->last_name = ($lname)?$lname:'N/A'; 
         $this->age = ($age)?$age:'N/A';
