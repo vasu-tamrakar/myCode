8aef2e30be3220b195d2edeb6992e6307bb10479
vasu-tamrakar <vasu.tamrakar@cssindiaonline.com>
2020-07-22 16:10:49 +0530

    changes

--- index.php
+++ index.php
@@ -3,4 +3,10 @@ include_once 'App.php';
 $user = new App();
 $user->setUser('VA','');
 $user->getUser();
+$user->getUser();
+$user->getUser();
+$e5q='Test';
+echo $test;
+echo 'Test';
+echo 'Test';
 ?>
