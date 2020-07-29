7a99724f4d0f7b18263f50c59460589dd9b480bf
vasu-tamrakar <vasu.tamrakar@cssindiaonline.com>
2020-07-29 18:00:52 +0530

    updated

--- main/mainTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'App.php';
-
-$user = new App();
-$user->setUser('Basu','BABA',20);
-echo $user->getfirst_name();
-echo $user->getlast_name();
-$data = $user->getUser();
-print_r($data);
